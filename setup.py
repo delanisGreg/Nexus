005bc67b95d384e22ae30c5660a099e0e9d19b98
delanisGreg <63797913+delanisGreg@users.noreply.github.com>
2020-06-14 08:50:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ from setuptools import setup
 
 setup(
         name='helloworls',
+        inclide_package_data=True,
         version='0.0.3',
         description='Hello!',
         py_module=["hello"],
         packages=['src'],
+        package_data={
+        'src': ['dockerfile'],
+        },
         package_dir={'src':'src'},
 )
