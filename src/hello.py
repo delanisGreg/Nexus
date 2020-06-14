4c4b01f187bd5b8b836d9492b2d77aad5ec2f6ee
delanisGreg <63797913+delanisGreg@users.noreply.github.com>
2020-06-14 12:00:31 +0300

    Update hello.py

--- src/hello.py
+++ src/hello.py
@@ -1,18 +1,12 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
+rom flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    name = "This is the route."
-    return name
-
-@app.route('/hello')
 def hello():
-    name = "Hello world!"
-    return name
+	return jsonify(
+{"about": "Hello there!"},
+)
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+	app.run(debug=True)
